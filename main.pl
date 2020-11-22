7b59e1dbcd3f0fa8d13da9b678ec9f96aa6b932a
Victor Cruz <vicdan215@gmail.com>
2020-11-22 12:12:54 -0600

    Add replace all function

--- main.pl
+++ main.pl
@@ -1,6 +1,15 @@
 %% New operators
 :- op(800,xfx,'=>').
 
+%=====List functions=====
+replaceAll(_, _, [], []).
+replaceAll(X, Y, [X|T], [Y|TRes]):-
+    replaceAll(X, Y, T, TRes).
+replaceAll(X, Y, [H|T], [H|TRes]):-
+    replaceAll(X, Y, T, TRes).
+
 %=====2b=====
 %addClassProperty(Class, Property, ActualKB, NewKB):-
+%    append(ActualKB, Res, NewKB)
 
+%addProperty()
