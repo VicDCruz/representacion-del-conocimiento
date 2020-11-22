02d930ccd55018a753a05589f123d2bcd5032578
Victor Cruz <vicdan215@gmail.com>
2020-11-22 13:05:02 -0600

    First draft of 2b

--- main.pl
+++ main.pl
@@ -8,8 +8,24 @@ replaceAll(X, Y, [X|T], [Y|TRes]):-
 replaceAll(X, Y, [H|T], [H|TRes]):-
     replaceAll(X, Y, T, TRes).
 
-%=====2b=====
-%addClassProperty(Class, Property, ActualKB, NewKB):-
-%    append(ActualKB, Res, NewKB)
+searchClass(Name, [], [])
+searchClass(Name, [class(Name, _, _, _, _)|T], class(Name, _, _, _, _)).
+searchClass(X, [Y|T], R):-
+    searchClass(X, T, R).
 
-%addProperty()
+%=====2b=====
+addClassProperty(Class, Name, Value, ActualKB, NewKB):-
+    searchClass(Class, ActualKB, class(Class, Parent, ActualProperties, R, O)),
+    appendProperty(ActualProperties, Name, Value, NewProperties).
+    replaceAll(class(Class, Parent, ActualProperties, R, O),
+        class(Class, Parent, NewProperties, R, O),
+        ActualKB, NewKB),
+
+appendProperty(ActualProperties, Name, yes, NewProperties):-
+	append(ActualProperties, [[Name, 0]], NewProperties).
+
+appendProperty(ActualProperties, Name, no, NewProperties):-
+	append(ActualProperties, [[not(Name), 0]], NewProperties).
+
+appendProperty(ActualProperties, Name, Value, NewProperties):-
+	append(ActualProperties, [[Name=>Value, 0]], NewProperties).
