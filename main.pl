cb8e5eaeb727e1ba0b01b56990a81c0fd19ece27
Victor Cruz <vicdan215@gmail.com>
2020-12-01 18:10:20 -0600

    Nuevos cambios

--- main.pl
+++ main.pl
@@ -2,24 +2,38 @@
 :- op(800,xfx,'=>').
 
 %=====List functions=====
+%------------------------------
+% Reemplazar todas las coincidencias
+%------------------------------
 replaceAll(_, _, [], []).
 replaceAll(X, Y, [X|T], [Y|TRes]):-
     replaceAll(X, Y, T, TRes).
 replaceAll(X, Y, [H|T], [H|TRes]):-
     replaceAll(X, Y, T, TRes).
 
-searchClass(Name, [], [])
-searchClass(Name, [class(Name, _, _, _, _)|T], class(Name, _, _, _, _)).
-searchClass(X, [Y|T], R):-
-    searchClass(X, T, R).
+%------------------------------
+% Checar si el elemento existe
+%------------------------------
+existsElement(X,[X|_]).
+existsElement(X,[_|T]):-
+	existsElement(X,T).
 
-%=====2b=====
-addClassProperty(Class, Name, Value, ActualKB, NewKB):-
-    searchClass(Class, ActualKB, class(Class, Parent, ActualProperties, R, O)),
-    appendProperty(ActualProperties, Name, Value, NewProperties).
-    replaceAll(class(Class, Parent, ActualProperties, R, O),
-        class(Class, Parent, NewProperties, R, O),
-        ActualKB, NewKB),
+%------------------------------
+% Agregar nueva propiedad a una clase:
+%------------------------------
+addClassProperty(Class, Name, Value):-
+    open_kb('kb.txt',ActualKB),
+    appendProperty(
+        ActualProperties,
+        Name,
+        Value,
+        NewProperties),
+    replaceAll(
+		class(Class, Parent, ActualProperties, R, O),
+		class(Class, Parent, NewProperties, R, O),
+		ActualKB,
+		NewKB),
+    save_kb('nueva_kb.txt', NewKB).
 
 appendProperty(ActualProperties, Name, yes, NewProperties):-
 	append(ActualProperties, [[Name, 0]], NewProperties).
@@ -29,3 +43,23 @@ appendProperty(ActualProperties, Name, no, NewProperties):-
 
 appendProperty(ActualProperties, Name, Value, NewProperties):-
 	append(ActualProperties, [[Name=>Value, 0]], NewProperties).
+
+%------------------------------
+% Agregar nueva propiedad a un objeto:
+%------------------------------
+addObjectProperty(Object, NewProperty, Value) :-
+    open_kb('kb.txt', ActualKB)
+	existsElement([id=>Object, Properties, Relations], Objects),
+	replaceAll(
+        [id=>Object, Properties, Relations],
+        [id=>Object, NewProperties, Relations],
+        Objects,
+        NewObjects),
+	replaceAll(
+        class(Class, Parent, P, R, Objects),
+        class(Class, Parent, P, R, NewObjects),
+        ActualKB,
+        NewKB),
+	appendProperty(Properties, NewProperty, Value, NewProperties).
+    save_kb('nueva_kb.txt', NewKB).
+
